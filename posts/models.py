d928d3da0c47334d276464ef3b184d1f4ac0b08e
Suyog738 <suyogacharya55555@gmail.com>
2024-10-31 20:31:12 +0200

    Updating table for image

--- posts/models.py
+++ posts/models.py
@@ -6,4 +6,9 @@ class Post(models.Model):
     body =  models.TextField()
     slug = models.SlugField()
     date = models.DateTimeField(auto_now_add=True)
+    banner = models.ImageField(blank=True)
+   
+def  __str__(self):
+    return self.title
+
    
